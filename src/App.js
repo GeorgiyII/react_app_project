42c6ba4bd2fd510bf103766e3a2f38cdb8050c00
GeorgiyII <gosha1104@gmail.com>
2020-08-16 14:09:09 +0300

    added routers

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
 import React from 'react';
+import {
+    Route,
+    Switch,
+    Redirect,
+    withRouter
+} from "react-router-dom"
+import Calculators from './components/calculators_prev/calculatorsComponent'
 
-function App() {
-  return (
-    <div> Hello world</div>
-  );
+class App extends React.Component {
+
+    render() {
+        return (
+            <div className="App">
+                <Switch>
+                    <Route exact path='/calculators' component={Calculators} />
+                    <Route path='/calculators' component={Calculators} />
+                    <Route path='/standards' component={Calculators} />
+                    <Route path='/standards' component={Calculators} />
+                    <Redirect from='/' to='/calculators'/>
+                </Switch>
+            </div>
+        );
+    }
 }
 
 export default App;
